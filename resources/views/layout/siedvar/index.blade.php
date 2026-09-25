135b373915b00c3d7c830212bcd8bbdd9a837e5a
JuanBautista <ab227748@gmail.com>
2026-09-24 22:31:56 -0600

    💄 SIedvar UI Improvement

--- resources/views/layout/siedvar/index.blade.php
+++ resources/views/layout/siedvar/index.blade.php
@@ -1,15 +1,32 @@
 <div class="col-md-2">
     <div class="card shadow">
-        <div class="card-header fw-bold">
-            Zenith X1 Pro
+        <div class="card-header">
+            <div class="row">
+                <nav class="navbar bg-body-tertiary">
+                    <div class="container-fluid fw-normal">
+                        <a class="navbar-brand ">
+                            <img src="img/Perfil.png" width="55" height="50"
+                                class=" d-inline-block align-text-top ">
+                        </a>
+                    </div>
+                </nav>
+                <p class="fs-6 fw-bolder">Panel Cliente</p>
+            </div>
         </div>
         <div class="card-body">
             <div class="list-group list-group-flush">
-                <button type="button" class="list-group-item list-group-item-action">Resumen</button>
-                <button type="button" class="list-group-item list-group-item-action">Especificaciones</button>
-                <button type="button" class="list-group-item list-group-item-action">Camaras</button>
-                <button type="button" class="list-group-item list-group-item-action">Bateria</button>
-                <button type="button" class="list-group-item list-group-item-action">Comparativa</button>
+                <button type="button" class="list-group-item list-group-item-action">Mi perfil</button>
+                <button type="button" class="list-group-item list-group-item-action">Mis citas</button>
+                <button type="button" class="list-group-item list-group-item-action">Mi contacto</button>
+            </div>
+        </div>
+        <div class="card-footer">
+            <div class="container-fluid">
+                <p class="fw-semibold">Horario Laboral</p>
+                <div class="row fw-light">
+                <p>Lun a Sab: 9:30 - 8:30</p>
+                <p>Av.central742 </p>
+                </div>
             </div>
         </div>
     </div>
